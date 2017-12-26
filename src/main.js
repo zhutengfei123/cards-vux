0ced34087b2be3ca27c980db41838595e40d19e5
fsx950223 <fsx950223@outlook.com>
2017-12-26 15:26:56 +0800

    feat(less): add less config

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import FastClick from 'fastclick'
+
 FastClick.attach(document.body)
 Vue.config.productionTip = false
 
