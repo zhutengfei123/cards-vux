ed63e514f3d5fab78e1a0b253b0f4f4c7e1acf65
fsx950223 <fsx950223@outlook.com>
2018-01-10 15:57:00 +0800

    feat(intention): add intention pages

---
+++ src/components/intention/submit.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='submit'>
+        <l-input v-for="(item,index) of list" :key="index" :textarea="item.textarea" :type="item.type" :placeholder="item.placeholder" :label="item.label" v-model="item.value"></l-input>
+    </div>
+</template>
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+import LInput from '../common/input'
+@Component({
+  components: {LInput}
+})
+export default class IntentionSubmit extends Vue {
+    list=[{
+      label: '姓名',
+      placeholder: '请填写姓名',
+      value: '',
+      textarea: false
+    }]
+}
+</script>
+<style lang="less" scoped>
+.submit{
+
+}
+</style>
