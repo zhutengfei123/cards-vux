84b15458dd53fd42dd241d1d30dee78f81a94937
fsx950223 <fsx950223@outlook.com>
2018-01-12 17:24:58 +0800

    feat(user): add user vuex module

--- src/vuex/modules/user.js
+++ src/vuex/modules/user.js
@@ -2,7 +2,6 @@ import { axios } from '../../js';
 // 用户 Store
 const state = {
   user: null,
-  logined: false,
   token: localStorage.getItem('token'),
   account: '',
   company: '',
@@ -13,25 +12,76 @@ const state = {
 };
 
 const actions = {
-  async login () {},
-  async register () {},
-  async getUserInfo ({commit}) {
-    const {result, status: {code, msg}} = await axios.get('/site/user-info');
+  async login ({commit, rootState}, {mobile, password}) {
+    const {result, status: {code, msg}} = await axios.post('/site/login', {
+      store_id: rootState.global.storeId,
+      mobile,
+      type: 1,
+      pwd: password
+    });
     if (code === '00000') {
-      commit('setUserInfo', result);
+      commit('setToken', result.token);
     } else {
       return msg;
     }
+  },
+  async register ({commit, rootState}, {mobile, password, company, code}) {
+    const {result, status} = await axios.post('/site/register', {
+      store_id: rootState.global.storeId,
+      mobile,
+      password,
+      company,
+      code
+    });
+    if (status.code === '00000') {
+      commit('setToken', result.token);
+    } else {
+      return status.msg;
+    }
+  },
+  async getInfo ({commit}) {
+    const {result, status: {code, msg}} = await axios.get('/site/user-info');
+    if (code === '00000') {
+      commit('setInfo', {
+        company: result.company,
+        realname: result.realname,
+        avatar: result.head_pic,
+        level: result.level_name,
+        balance: result.balance
+      });
+    } else {
+      return msg;
+    }
+  },
+  async updateInfo ({commit, state}, {realname, avatar}) {
+    const {status: {code, msg}} = await axios.post('/site/update-info', {realname, head_pic: avatar});
+    if (code === '00000') {
+      commit('setInfo', {
+        ...state,
+        realname,
+        avatar
+      });
+    } else {
+      return msg;
+    }
+  },
+  async resetPwd ({commit, rootState}, {mobile, password, code}) {
+    const {status: {msg}} = await axios.post('/site/reset-pwd', {
+      store_id: rootState.global.storeId,
+      mobile,
+      password,
+      code
+    });
+    return msg;
   }
 };
 
 const mutations = {
-  setUserInfo (state, data) {
-    state.company = data.company;
-    state.realname = data.realname;
-    state.avatar = data.head_pic;
-    state.level = data.level_name;
-    state.balance = data.balance;
+  setInfo (state, data) {
+    state = data;
+  },
+  setToken (state, token) {
+    state.token = token;
   }
 };
 
