0362b48314f18d4aa1ad38f4d87b52cfd05c10e8
Eric <1932945408@qq.com>
2018-01-19 20:24:56 +0800

    修改

--- src/components/classification/index.vue
+++ src/components/classification/index.vue
@@ -2,6 +2,15 @@
   <div class="classification">
     <tab bar-active-color="transparent" class="tab" ref="topBar">
       <tab-item :selected="index===0?true:false" v-for="(item, index) in tabsList" :key="index" @on-item-click="handleClickTabs(index)">{{item.title}}<span :class="index===isActive?'active':''" class="app-icon">&#xe611;</span></tab-item>
+      <div class="drop-down-box" v-show="isShowBox">
+        <div class="drop-t">
+          <div @click="handleSelectCon(index)" :class="index===isActive1?'active1':''" class="con-box" v-for="(item, index) in categoryData[isActive].children" :key="index">{{item.name}}</div>
+        </div>
+        <div class="drop-b">
+          <span class="drop-b-l">重 置</span>
+          <span class="drop-b-r">确 定</span>
+        </div>
+      </div>
     </tab>
     <div class="card-list-wrap">
       <div class="card-list" v-for="(item, index) in initData.list" :key="index">
@@ -11,6 +20,7 @@
         <div class="card-list-btn"><x-button mini>加入购物车</x-button></div>
       </div>
     </div>
+    <div v-show="isShowBox" class="msk-box"></div>
   </div>
 </template>
 <script>
@@ -31,21 +41,38 @@ const GlobalState = namespace('global', State);
 })
 export default class Classification extends Vue {
   @ProductsState initData
+  @ProductsState categoryData
   @ProductsAction init
+  @ProductsAction initCategoryData
   @ProductsMutation getInitData
   @GlobalState storeId
   currentPage = 1
+  isShowBox = false
   isActive = 0
+  isActive1 = 0
   tabsList = [
     {title: '主题'},
     {title: '对象'},
     {title: '场合'},
     {title: '排序'}
   ]
+  handleSelectCon (n) {
+    this.isActive1 = n;
+  }
   handleClickTabs (n) {
     this.isActive = n;
+    this.isShowBox = !this.isShowBox;
   }
   created () {
+    this.initial();
+    const params = {};
+    this.initCategoryData(params).then(msg => {
+      if (msg) {
+        this.$vux.toast.text(msg, 'middle');
+      }
+    });
+  }
+  initial () {
     const params = {
       'store_id': this.storeId,
       'page': this.currentPage,
@@ -65,8 +92,61 @@ export default class Classification extends Vue {
 <style lang="less" scoped>
 .classification{
   width: 100%;
+  .msk-box {
+    position: fixed;
+    width: 100%;
+    height: 60%;
+    left: 0;
+    bottom: 0;
+    opacity: 0.7;
+    background: #000000;
+    z-index: 999;
+  }
+  .con-box {
+    height: 0.44rem;
+    width: 50%;
+    float: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #F6F6F6;
+  }
+  .drop-t {
+    height: 1.35rem;
+    overflow-y: auto;
+    overflow-x: hidden;
+    box-sizing: border-box;
+  }
+  .drop-b {
+    display: flex;
+    box-sizing: border-box;
+  }
+  .drop-b-l, .drop-b-r {
+    height: 0.44rem;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .drop-b-r {
+    background: #B79E74;
+    color: #ffffff;
+  }
+  .drop-down-box {
+    background: #ffffff;
+    position: absolute;
+    width: 100%;
+    top: 0.44rem;
+    left: 0;
+    border-bottom: 1px solid #F6F6F6;
+    box-sizing: border-box;
+  }
   .active {
     color: #B79E74 !important;
+  }
+  .active1 {
+    color: #B79E74 !important;
+    border-bottom: 1px solid #B79E74 !important;
   }
   .app-icon {
     font-size: 0.15rem;
--- src/vuex/modules/products.js
+++ src/vuex/modules/products.js
@@ -1,7 +1,10 @@
 import {axios} from '../../js';
 // import qs from 'qs';
 const state = {
-  initData: []
+  initData: [],
+  categoryData: [
+    {children: []}
+  ]
 };
 const actions = {
   async init ({commit, rootState}, params) {
@@ -11,11 +14,22 @@ const actions = {
     } else {
       return msg;
     }
+  },
+  async initCategoryData ({commit, rootState}, params) {
+    const { result, status: {code, msg} } = await axios.get('/index.php/card-category', {'params': params});
+    if (code === '00000') {
+      commit('getCategoryData', result);
+    } else {
+      return msg;
+    }
   }
 };
 const mutations = {
   getInitData (state, data) {
     state.initData = data;
+  },
+  getCategoryData (state, data) {
+    state.categoryData = data;
   }
 };
 export default {
