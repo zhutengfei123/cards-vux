ea1eff9f6ed9fc7f26a1f84785f366ab19c8999d
Eric <1932945408@qq.com>
2018-03-26 14:08:58 +0800

    修改

--- src/components/intention/intentionList.vue
+++ src/components/intention/intentionList.vue
@@ -2,7 +2,7 @@
     <div class="cart">
         <div class="cart-top">
             <span>共{{intentionList.num||0}}件</span>
-            <span class="cart-edit" @click="handleCartEdit(isEdit)">{{isEdit?'完成':'编辑'}}</span>
+            <span class="cart-edit" @click="handleCartEdit(isEdit)">{{isEdit?'编辑':'完成'}}</span>
         </div>
         <div class="cart-con" v-for="(item, index) in intentionList.list" :key="index">
             <div class="con-top">
@@ -27,7 +27,7 @@
         <div class="cart-foot">
             <check-icon :value.sync="intentionList.is_all_selected===1?true:false" @click.native="handleSelectAll">全选</check-icon>
             <span>合计：<span class="bottom-l">￥{{intentionList.goods_total_price}}</span></span>
-            <span class="settlement" @click="handleClick">{{isEdit?'删除':'提交意向单'}}</span>
+            <span class="settlement" @click="handleClick">{{isEdit?'提交意向单':'删除'}}</span>
         </div>
     </div>
 </template>
@@ -46,7 +46,7 @@ const ProductsAction = namespace('products', Action);
 export default class Cart extends Vue {
   @ProductsState intentionList;
   @ProductsAction initGetIntentionList;
-  isEdit = false;
+  isEdit = true;
   myTempData = []
   computedPrice () {
     let totalPrice = 0;
@@ -158,7 +158,7 @@ export default class Cart extends Vue {
     this.computedPrice();
   }
   handleClick () {
-    if (this.isEdit) {
+    if (!this.isEdit) {
       let arr = this.intentionList.list;
       let len = this.intentionList.list.length;
       for (let i = 0; i < len; i++) {
